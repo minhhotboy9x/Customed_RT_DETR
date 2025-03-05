a69b06d228b7b8ebaf9542843143bff9d85c13c1
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2025-03-05 15:30:38 +0700

    init commit

---
+++ sandbox.py
@@ -0,0 +1,151 @@
+import os 
+import sys
+import argparse
+import time 
+import json
+import datetime
+import torch
+
+# import src.misc.dist as dist 
+from src.core import YAMLConfig
+from src.misc import dist
+from src.solver.det_solver import DetSolver 
+from src.solver.det_engine import *
+from src.solver import TASKS
+from src.data import get_coco_api_from_dataset
+from argparse import Namespace
+
+class CustomedDetSolver(DetSolver):
+    def fit(self, ):
+        print("Start training")
+        self.train()
+
+        args = self.cfg 
+        
+        n_parameters = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+        print('number of params:', n_parameters)
+
+        base_ds = get_coco_api_from_dataset(self.val_dataloader.dataset)
+        # best_stat = {'coco_eval_bbox': 0, 'coco_eval_masks': 0, 'epoch': -1, }
+        best_stat = {'epoch': -1, }
+
+        start_time = time.time()
+        for epoch in range(self.last_epoch + 1, args.epoches):
+            if dist.is_dist_available_and_initialized():
+                self.train_dataloader.sampler.set_epoch(epoch)
+            
+            train_stats = train_one_epoch(
+                self.model, self.criterion, self.train_dataloader, self.optimizer, self.device, epoch,
+                args.clip_max_norm, print_freq=args.log_step, ema=self.ema, scaler=self.scaler)
+
+            self.lr_scheduler.step()
+            
+            # if self.output_dir:
+            #     checkpoint_paths = [self.output_dir / 'checkpoint.pth']
+            #     # extra checkpoint before LR drop and every 100 epochs
+            #     if (epoch + 1) % args.checkpoint_step == 0:
+            #         checkpoint_paths.append(self.output_dir / f'checkpoint{epoch:04}.pth')
+            #     for checkpoint_path in checkpoint_paths:
+            #         dist.save_on_master(self.state_dict(epoch), checkpoint_path)
+
+            module = self.ema.module if self.ema else self.model
+            test_stats, coco_evaluator = evaluate(
+                module, self.criterion, self.postprocessor, self.val_dataloader, base_ds, self.device, self.output_dir
+            )
+
+            # TODO 
+            for k in test_stats.keys():
+                checkpoint_path = self.output_dir / f'last.pth'
+                best_path = self.output_dir / f'best.pth'
+                dist.save_on_master(self.state_dict(epoch), checkpoint_path)
+                if k in best_stat:
+                    best_stat['epoch'] = epoch if test_stats[k][0] > best_stat[k] else best_stat['epoch']
+                    # best_stat[k] = max(best_stat[k], test_stats[k][0])
+                    if best_stat[k] < test_stats[k][0]:
+                        best_stat[k] = test_stats[k][0]
+                        if self.output_dir:
+                            dist.save_on_master(self.state_dict(epoch), best_path)
+                else:
+                    best_stat['epoch'] = epoch
+                    best_stat[k] = test_stats[k][0]
+                    if self.output_dir:
+                        dist.save_on_master(self.state_dict(epoch), best_path)
+            print('best_stat: ', best_stat)
+
+
+            log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
+                        **{f'test_{k}': v for k, v in test_stats.items()},
+                        'epoch': epoch,
+                        'n_parameters': n_parameters}
+
+            if self.output_dir and dist.is_main_process():
+                with (self.output_dir / "log.txt").open("a") as f:
+                    f.write(json.dumps(log_stats) + "\n")
+
+                # for evaluation logs
+                if coco_evaluator is not None:
+                    (self.output_dir / 'eval').mkdir(exist_ok=True)
+                    if "bbox" in coco_evaluator.coco_eval:
+                        filenames = ['latest.pth']
+                        if epoch % 50 == 0:
+                            filenames.append(f'{epoch:03}.pth')
+                        for name in filenames:
+                            torch.save(coco_evaluator.coco_eval["bbox"].eval,
+                                    self.output_dir / "eval" / name)
+
+        total_time = time.time() - start_time
+        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+        print('Training time {}'.format(total_time_str))
+    
+    def val_with_query(self, ):
+        self.eval()
+
+        base_ds = get_coco_api_from_dataset(self.val_dataloader.dataset)
+        
+        module = self.ema.module if self.ema else self.model
+        test_stats, coco_evaluator = evaluate_with_query(module, self.criterion, self.postprocessor,
+                self.val_dataloader, base_ds, self.device, self.output_dir)
+                
+        if self.output_dir:
+            dist.save_on_master(coco_evaluator.coco_eval["bbox"].eval, self.output_dir / "eval.pth")
+        
+        return
+    
+if __name__ == '__main__':
+    # download pretrained model: https://github.com/lyuwenyu/storage/releases/download/v0.1/rtdetr_r18vd_dec3_6x_coco_from_paddle.pth
+    args = Namespace(
+                    config='configs/rtdetr/rtdetr_r18vd_6x_coco_customed.yml', 
+                    # config='configs/rtdetr/rtdetr_r18vd_6x_coco.yml', 
+                    resume=None, 
+                    tuning='rtdetr_r18vd_dec3_6x_coco_from_paddle.pth', 
+                    test_only=False, 
+                    amp=True, 
+                    seed=None)
+    # dist.init_distributed()
+    # if args.seed is not None:
+    #     dist.set_seed(args.seed)
+
+    assert not all([args.tuning, args.resume]), \
+        'Only support from_scrach or resume or tuning at one time'
+
+    cfg = YAMLConfig(
+        args.config,
+        resume=args.resume, 
+        use_amp=args.amp,
+        tuning=args.tuning
+    )
+    cfg.epoches = 300
+    # print(list(vars(cfg).keys()))
+    solver = CustomedDetSolver(cfg)
+    solver.train()
+
+    for batch in solver.train_dataloader:
+        input = batch[0].to(solver.device)
+        targets = [{k: v.to(solver.device) for k, v in t.items()} for t in batch[1]]
+        break    
+    out = solver.model(input, targets)
+
+    if args.test_only:
+        solver.val_with_query()
+    else:
+        solver.fit()
---
+++ src/zoo/rtdetr/customed_postprocessor.py
@@ -0,0 +1,165 @@
+"""by lyuwenyu
+"""
+
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F 
+
+import torchvision
+
+from src.core import register
+
+
+__all__ = ['CustomedPostProcessor']
+
+
+@register
+class CustomedPostProcessor(nn.Module):
+    '''
+    box output in format 
+    '''
+    __share__ = ['num_classes', 'use_focal_loss', 'num_top_queries', 'remap_mscoco_category', 'conf']
+    
+    def __init__(self, num_classes=80, use_focal_loss=True, num_top_queries=300, remap_mscoco_category=False, conf=0.25) -> None:
+        super().__init__()
+        self.use_focal_loss = use_focal_loss
+        self.num_top_queries = num_top_queries
+        self.num_classes = num_classes
+        self.remap_mscoco_category = remap_mscoco_category 
+        self.deploy_mode = False 
+        self.conf = conf
+
+    def extra_repr(self) -> str:
+        return f'use_focal_loss={self.use_focal_loss}, num_classes={self.num_classes}, num_top_queries={self.num_top_queries}'
+    
+    # def forward(self, outputs, orig_target_sizes):
+    def forward(self, outputs, orig_target_sizes):
+
+        logits, boxes = outputs['pred_logits'], outputs['pred_boxes']
+        # orig_target_sizes = torch.stack([t["orig_size"] for t in targets], dim=0)        
+
+        bbox_pred = torchvision.ops.box_convert(boxes, in_fmt='cxcywh', out_fmt='xyxy')
+        bbox_pred *= orig_target_sizes.repeat(1, 2).unsqueeze(1)
+        
+        # fix wrong postprocess
+        if self.use_focal_loss:
+            scores = F.sigmoid(logits)
+        else:
+            scores = F.softmax(logits, dim=-1)
+        
+        scores, labels = scores.max(dim=-1)
+        boxes = bbox_pred
+        if scores.shape[1] > self.num_top_queries:
+            scores, index = torch.topk(scores, self.num_top_queries, dim=-1)
+            labels = torch.gather(labels, dim=1, index=index)
+            boxes = torch.gather(boxes, dim=1, index=index.unsqueeze(-1).tile(1, 1, boxes.shape[-1]))
+
+        # TODO for onnx export
+        if self.deploy_mode:
+            return labels, boxes, scores
+
+        # TODO
+        if self.remap_mscoco_category:
+            from ...data.coco import mscoco_label2category
+            labels = torch.tensor([mscoco_label2category[int(x.item())] for x in labels.flatten()])\
+                .to(boxes.device).reshape(labels.shape)
+
+        results = []
+        for lab, box, sco in zip(labels, boxes, scores):
+            mask = sco > self.conf
+            lab = lab[mask]
+            box = box[mask]
+            sco = sco[mask]
+            result = dict(labels=lab, boxes=box, scores=sco)
+            results.append(result)
+        
+        return results
+        
+
+    def deploy(self, ):
+        self.eval()
+        self.deploy_mode = True
+        return self 
+
+    @property
+    def iou_types(self, ):
+        return ('bbox', )
+
+@register
+class CustomedPostProcessor2(nn.Module):
+    '''
+    box output in format 
+    '''
+    __share__ = ['num_classes', 'use_focal_loss', 'num_top_queries', 'remap_mscoco_category', 'selected_query']
+    
+    def __init__(self, num_classes=80, use_focal_loss=True, num_top_queries=300, remap_mscoco_category=False, selected_query=True) -> None:
+        super().__init__()
+        self.use_focal_loss = use_focal_loss
+        self.num_top_queries = num_top_queries
+        self.num_classes = num_classes
+        self.remap_mscoco_category = remap_mscoco_category 
+        self.deploy_mode = False 
+        self.selected_query = selected_query
+
+    def extra_repr(self) -> str:
+        return f'use_focal_loss={self.use_focal_loss}, num_classes={self.num_classes}, num_top_queries={self.num_top_queries}'
+    
+    # def forward(self, outputs, orig_target_sizes):
+    def forward(self, outputs, orig_target_sizes):
+
+        logits, boxes = outputs['pred_logits'], outputs['pred_boxes']
+        # orig_target_sizes = torch.stack([t["orig_size"] for t in targets], dim=0)        
+
+        bbox_pred = torchvision.ops.box_convert(boxes, in_fmt='cxcywh', out_fmt='xyxy')
+        bbox_pred *= orig_target_sizes.repeat(1, 2).unsqueeze(1)
+        num_repeated_queries = 0
+
+        if self.use_focal_loss:
+            scores = F.sigmoid(logits)
+            scores, index = torch.topk(scores.flatten(1), self.num_top_queries, axis=-1)
+            labels = index % self.num_classes
+            index = index // self.num_classes
+            boxes = bbox_pred.gather(dim=1, index=index.unsqueeze(-1).repeat(1, 1, bbox_pred.shape[-1]))
+            
+            if self.selected_query:
+                one_hot = torch.nn.functional.one_hot(index)
+                freq = one_hot.sum(dim=1)
+                duplicate_mask = freq > 1
+                num_repeated_queries += duplicate_mask.sum()
+
+            
+        else:
+            scores = F.softmax(logits)[:, :, :-1]
+            scores, labels = scores.max(dim=-1)
+            boxes = bbox_pred
+            if scores.shape[1] > self.num_top_queries:
+                scores, index = torch.topk(scores, self.num_top_queries, dim=-1)
+                labels = torch.gather(labels, dim=1, index=index)
+                boxes = torch.gather(boxes, dim=1, index=index.unsqueeze(-1).tile(1, 1, boxes.shape[-1]))
+
+        # TODO for onnx export
+        if self.deploy_mode:
+            return labels, boxes, scores
+
+        # TODO
+        if self.remap_mscoco_category:
+            from ...data.coco import mscoco_label2category
+            labels = torch.tensor([mscoco_label2category[int(x.item())] for x in labels.flatten()])\
+                .to(boxes.device).reshape(labels.shape)
+
+        results = []
+        for lab, box, sco in zip(labels, boxes, scores):
+            result = dict(labels=lab, boxes=box, scores=sco)
+            results.append(result)
+        
+        return results, num_repeated_queries
+        
+
+    def deploy(self, ):
+        self.eval()
+        self.deploy_mode = True
+        return self 
+
+    @property
+    def iou_types(self, ):
+        return ('bbox', )
