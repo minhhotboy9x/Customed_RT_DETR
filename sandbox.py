a1071b8c43cfc018fc3ae5e2a5e5f310b4009536
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2025-04-15 11:55:52 +0700

    only leverage logit

--- sandbox.py
+++ sandbox.py
@@ -115,12 +115,12 @@ if __name__ == '__main__':
     # download pretrained model: https://github.com/lyuwenyu/storage/releases/download/v0.1/rtdetr_r18vd_dec3_6x_coco_from_paddle.pth
     args = Namespace(
                     # config='configs/rtdetr/rtdetr_r18vd_6x_coco_customed.yml', 
-                    config='configs/rtdetr/rtdetr_r18vd_6x_coco.yml', 
+                    # config='configs/rtdetr/rtdetr_r18vd_6x_coco.yml', 
                     # config='configs/rtdetr/rtdetr_r50vd_6x_coco.yml', 
-                    # config='configs/rtdetr/rtdetr_r50vd_6x_coco_customed.yml', 
+                    config='configs/rtdetr/rtdetr_r50vd_6x_coco_customed.yml', 
                     resume=None, 
-                    tuning='rtdetr_r18vd_dec3_6x_coco_from_paddle.pth', 
-                    # tuning='rtdetr_r50vd_6x_coco_from_paddle.pth', 
+                    # tuning='rtdetr_r18vd_dec3_6x_coco_from_paddle.pth', 
+                    tuning='rtdetr_r50vd_6x_coco_from_paddle.pth', 
                     test_only=True, 
                     amp=True, 
                     seed=None)
--- src/zoo/rtdetr/customed_decoder.py
+++ src/zoo/rtdetr/customed_decoder.py
@@ -19,14 +19,26 @@ __all__ = ['CustomedRTDETRTransformer']
 
 @register
 class CustomedRTDETRTransformer(RTDETRTransformer):
+    __share__ = ['num_classes']
 
     def _get_decoder_input(self,
                            memory,
                            spatial_shapes,
                            denoising_class=None,
                            denoising_bbox_unact=None,
-                           leverage_class_anchors = [0.2]):
+                           leverage_class_anchors = [0.1],
+                           targets=None,):
         bs, _, _ = memory.shape
+
+        # target to detect small objects
+        small_mask = torch.zeros(bs, device=memory.device)
+        if targets is not None:
+            for i in range(len(targets)):
+                area = targets[i]['area']
+                if area.numel() > 0 and (area < 1024.0).any():
+                    small_mask[i] = 1
+    
+        small_mask = small_mask.unsqueeze(1).unsqueeze(1)
         # prepare input for decoder
         if self.training or self.eval_spatial_size is None:
             anchors, valid_mask = self._generate_anchors(spatial_shapes, device=memory.device)
@@ -45,7 +57,8 @@ class CustomedRTDETRTransformer(RTDETRTransformer):
         for i in range(min(len(leverage_class_anchors), len(spatial_shapes))):
             start_id_anchor = 0 if i == 0 else spatial_shapes[i-1][0] * spatial_shapes[i-1][1]
             end_id_anchor = spatial_shapes[i][0] * spatial_shapes[i][1]
-            enc_outputs_class_tmp[:, start_id_anchor:end_id_anchor, :] += leverage_class_anchors[i]
+            enc_outputs_class_tmp[:, start_id_anchor:end_id_anchor, :] += \
+                leverage_class_anchors[i] * small_mask
 
         _, topk_ind = torch.topk(enc_outputs_class_tmp.max(-1).values, self.num_queries, dim=1)
         
@@ -73,3 +86,56 @@ class CustomedRTDETRTransformer(RTDETRTransformer):
 
         return target, reference_points_unact.detach(), enc_topk_bboxes, enc_topk_logits
 
+
+    def forward(self, feats, targets=None):
+        # input projection and embedding
+        (memory, spatial_shapes, level_start_index) = self._get_encoder_input(feats)
+        
+        # prepare denoising training
+        if self.training and self.num_denoising > 0:
+            denoising_class, denoising_bbox_unact, attn_mask, dn_meta = \
+                get_contrastive_denoising_training_group(targets, \
+                    self.num_classes, 
+                    self.num_queries, 
+                    self.denoising_class_embed, 
+                    num_denoising=self.num_denoising, 
+                    label_noise_ratio=self.label_noise_ratio, 
+                    box_noise_scale=self.box_noise_scale, )
+        else:
+            denoising_class, denoising_bbox_unact, attn_mask, dn_meta = None, None, None, None
+
+        target, init_ref_points_unact, enc_topk_bboxes, enc_topk_logits = \
+            self._get_decoder_input(memory, spatial_shapes, 
+                                    denoising_class, 
+                                    denoising_bbox_unact,
+                                    targets=targets,)
+
+        # decoder
+        out_bboxes, out_logits = self.decoder(
+            target,
+            init_ref_points_unact,
+            memory,
+            spatial_shapes,
+            level_start_index,
+            self.dec_bbox_head,
+            self.dec_score_head,
+            self.query_pos_head,
+            attn_mask=attn_mask)
+
+        if self.training and dn_meta is not None:
+            dn_out_bboxes, out_bboxes = torch.split(out_bboxes, dn_meta['dn_num_split'], dim=2)
+            dn_out_logits, out_logits = torch.split(out_logits, dn_meta['dn_num_split'], dim=2)
+
+        out = {'pred_logits': out_logits[-1], 'pred_boxes': out_bboxes[-1]}
+
+        if self.training and self.aux_loss:
+            out['aux_outputs'] = self._set_aux_loss(out_logits[:-1], out_bboxes[:-1])
+            out['aux_outputs'].extend(self._set_aux_loss([enc_topk_logits], [enc_topk_bboxes]))
+            
+            if self.training and dn_meta is not None:
+                out['dn_aux_outputs'] = self._set_aux_loss(dn_out_logits, dn_out_bboxes)
+                out['dn_meta'] = dn_meta
+
+        return out
+
+    
